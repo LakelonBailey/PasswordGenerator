6ff79c3ae4ecc585180b82857d39807956936e10
Lake Bailey <lake.bailey@icloud.com>
2022-05-03 14:38:37 -0400

    added remaining window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 // Assignment code here
 function generatePassword() {
   var numChar = window.prompt("Choose a password length from 8-128 characters, inclusively: ");
+  var lCase = window.confirm("Would you like to include lowercase letters?");
+  var uCase = window.confirm("Would you like to include uppercase letters?");
+  var numeric = window.confirm("Would you like to include numeric characters?");
+  var special = window.confirm("Would you like to include special characters?");
 
 }
 
